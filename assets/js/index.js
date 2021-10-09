aa7cb36663260fab3521415b273940a475db375a
BraianMG <braian.gonzales77@gmail.com>
2021-10-09 00:34:39 -0300

    skills section almost finished

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,10 @@ const navMenu = document.querySelector('.nav-menu')
 const nav = document.querySelector('.nav')
 const links = document.querySelectorAll('.nav-menu-link')
 const imgHamburger = document.querySelector('.menu-img')
+const blocks = document.querySelectorAll('.block')
+const titlesBlock = document.querySelectorAll('.title-block')
 
+// NAVBAR
 navHamburger.addEventListener('click', () => {
     navMenu.classList.toggle('nav-menu--active')
     nav.classList.toggle('nav--light')
@@ -25,6 +28,19 @@ window.addEventListener('scroll', () => {
     }else{
         console.log('transparente')
         nav.classList.remove('nav--change')
-    }
-    
+    }  
 })
+
+// ACCORDION
+titlesBlock.forEach( (titleBlock,i) => {
+
+    titlesBlock[i].addEventListener('click', () => {
+        
+        blocks.forEach( (block,j) => {
+            if(i!==j){
+                blocks[j].classList.remove('active')
+            }
+        })
+        blocks[i].classList.toggle('active')
+    })
+} )
