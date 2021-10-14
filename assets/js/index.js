1dd7cb262a13bdcec59040806cfdf2829a4c653a
BraianMG <braian.gonzales77@gmail.com>
2021-10-14 14:48:13 -0300

    finished contact form functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -75,3 +75,53 @@ tabs.forEach( tab => {
         tab.classList.add('qualification--show')
     })
 })
+
+// CONTACT ME
+const connection = 'https://sheet.best/api/sheets/58e84b38-45ed-4943-9c8e-6891f3c783b6'
+const form = document.querySelector('.contact-form');
+const success = document.querySelector('.form-message.success');
+const warning = document.querySelector('.form-message.warning');
+
+form.addEventListener('submit', async(e) => {
+	e.preventDefault();
+
+	try {
+		const respuesta = await fetch(connection, {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				"Name": form.name.value,
+				"Email": form.email.value,
+				"Project": form.project.value,
+                "Message": form.message.value
+			})
+		});
+
+		const contenido = await respuesta.json();
+        console.log(contenido)
+		form.classList.toggle('hidden')
+        success.classList.toggle('hidden')
+	} catch(error){
+        form.classList.toggle('hidden')
+        warning.classList.toggle('hidden')
+		console.log(error);
+	}
+});
+
+const inputsArea = document.querySelectorAll('.inputs-content')
+const inputs = document.querySelectorAll('.input-content')
+
+console.log(inputs)
+inputs.forEach( (input,i) => {
+
+    inputs[i].addEventListener('focus', () => {
+        inputsArea[i].classList.toggle('inputs-content-focus')
+    })
+
+    inputs[i].addEventListener('blur', () => {
+        inputsArea[i].classList.remove('inputs-content-focus')
+    })
+})
