1f752b551b886c8233e1c5cb277c66fc39a49620
BraianMG <braian.gonzales77@gmail.com>
2021-10-15 18:51:29 -0300

    user validation and added some effects

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,32 @@
+// NAVBAR
 const navHamburger = document.querySelector('.menu-hamburger')
 const navMenu = document.querySelector('.nav-menu')
 const nav = document.querySelector('.nav')
 const logo = document.querySelector('.logo')
 const links = document.querySelectorAll('.nav-menu-link')
 const imgHamburger = document.querySelector('.menu-img')
+// NAV Y SCROLLUP
+const scrollup = document.querySelector('.scrollup')
+// ACCORDION
 const blocks = document.querySelectorAll('.block')
 const titlesBlock = document.querySelectorAll('.title-block')
 const arrows = document.querySelectorAll('.arrow')
-const scrollup = document.querySelector('.scrollup')
+// QUALIFICATIONS
 const tabs = document.querySelectorAll('[data-target]')
 const tabContents = document.querySelectorAll('[data-content]')
+// CONTACT ME (FORM)
+const connection = 'https://sheet.best/api/sheets/58e84b38-45ed-4943-9c8e-6891f3c783b6'
+const form = document.querySelector('.contact-form');
+const submitButton = document.querySelector('form .button')
+const success = document.querySelector('.form-message.success')
+const warning = document.querySelector('.form-message.warning')
+const notice = document.querySelector('.notice')
+const loader = document.querySelector('.loader')
+// CONTACT ME (EFFECTS)
+const inputsArea = document.querySelectorAll('.inputs-content')
+const inputs = document.querySelectorAll('.input-content')
+
+
 
 // NAVBAR
 navHamburger.addEventListener('click', () => {
@@ -76,45 +93,51 @@ tabs.forEach( tab => {
     })
 })
 
-// CONTACT ME
-const connection = 'https://sheet.best/api/sheets/58e84b38-45ed-4943-9c8e-6891f3c783b6'
-const form = document.querySelector('.contact-form');
-const success = document.querySelector('.form-message.success');
-const warning = document.querySelector('.form-message.warning');
 
+// CONTACT ME (FORM)
 form.addEventListener('submit', async(e) => {
-	e.preventDefault();
+    e.preventDefault();
+    
+    loader.classList.toggle('hidden')
+    submitButton.classList.toggle('hidden')
 
-	try {
-		const respuesta = await fetch(connection, {
-			method: 'POST',
-			mode: 'cors',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				"Name": form.name.value,
-				"Email": form.email.value,
-				"Project": form.project.value,
-                "Message": form.message.value
-			})
-		});
+    // VALIDATION
+    if (form.name.value==='' || form.email.value==='' || form.project.value==='' || form.message.value===''){
+        loader.classList.toggle('hidden')
+        submitButton.classList.toggle('hidden')
+        notice.classList.remove('hidden')
 
-		const contenido = await respuesta.json();
-        console.log(contenido)
-		form.classList.toggle('hidden')
-        success.classList.toggle('hidden')
-	} catch(error){
-        form.classList.toggle('hidden')
-        warning.classList.toggle('hidden')
-		console.log(error);
-	}
+    }else{
+        notice.classList.add('hidden')
+
+        try {
+            const respuesta = await fetch(connection, {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "Name": form.name.value,
+                    "Email": form.email.value,
+                    "Project": form.project.value,
+                    "Message": form.message.value
+                })
+            });
+    
+            const contenido = await respuesta.json();
+            
+            form.classList.toggle('hidden')
+            success.classList.toggle('hidden')
+        } catch(error){
+            form.classList.toggle('hidden')
+            warning.classList.toggle('hidden')
+            console.log(error);
+        }
+    }
 });
 
-const inputsArea = document.querySelectorAll('.inputs-content')
-const inputs = document.querySelectorAll('.input-content')
-
-console.log(inputs)
+// CONTACT ME (EFFECTS)
 inputs.forEach( (input,i) => {
 
     inputs[i].addEventListener('focus', () => {
